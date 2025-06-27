45f7320d0f03f06460ab7bfbeedb744473c0ea55
Faris <alarifinmirzafaris@gmail.com>
2025-06-27 20:33:22 +0700

    Complete 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const letters = str.split("");
+    const reversedLetters = [];
+    for (let i = 0, length = letters.length; i < length; i++) {
+        reversedLetters.push(letters.pop());
+    }
+    return reversedLetters.join("");
 };
+
+console.log(reverseString("hello"))
 
 // Do not edit below this line
 module.exports = reverseString;
