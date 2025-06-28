b54d3dd1e13efa884655fdd2d30b7379585558af
Faris <alarifinmirzafaris@gmail.com>
2025-06-28 18:11:08 +0700

    Complete 10

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    return (year % 4 == 0) && (!(year % 100 == 0) || (year % 400 == 0))
 };
+
+/**
+ * All 100 is divisable by 4 so the constructor is
+ * year % 4 == 0
+ *      if divisable by 100
+ *          not
+ *      else if divisable by 400
+ */
 
 // Do not edit below this line
 module.exports = leapYears;
