8ea55db1e55b74218e8795b8cb61fd931af45fd0
Faris <alarifinmirzafaris@gmail.com>
2025-07-02 23:08:22 +0700

    Complete 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  if (n < 0) {
+    return "OOPS";
+  }
 
+  const result = [];
+  for (let i = 0; i <= n; i++) {
+    if (i === 0 || i === 1) {
+      result.push(i);
+    } else {
+      result.push(result[i - 1] + result[i - 2]);
+    }
+  }
+  return result.pop();
 };
 
 // Do not edit below this line
